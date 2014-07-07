94c9a386b15bd054453451e4b4459017aa250553
Thomas Anderson <thomas@myfreebyte.com>
2014-07-07 17:27:54 +0700

    setup tab active

---
+++ javascripts/app.js
@@ -0,0 +1,27 @@
+var main = function () {
+	"use strict";
+
+	$(".tabs a span").toArray().forEach(function (element) {
+		// create a link handle for this element
+		$(element).on("click", function() {
+
+			var $element = $(element);
+
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
+			$("main .content").empty();
+
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("First tab clicked!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("Second tab clicked!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("Third tab clicked!");
+			}
+
+			return false;
+		});
+	});
+};
+
+$(document).ready(main);
