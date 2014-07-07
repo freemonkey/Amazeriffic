cb51fe53cee2d16c1120e6fed339d3031696edc1
Thomas Anderson <thomas@myfreebyte.com>
2014-07-07 20:45:40 +0700

    Add tab content

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,27 +1,60 @@
 var main = function () {
 	"use strict";
 
-	$(".tabs a span").toArray().forEach(function (element) {
-		// create a link handle for this element
-		$(element).on("click", function() {
+	var toDos = [
+		"Finish writing this book",
+		"Take Gracie to the park",
+		"Answer emails",
+		"Prep for Monday's class",
+		"Make up some new ToDos",
+		"Get Groceries"
+	];
 
-			var $element = $(element);
+	$(".tabs a span").toArray().forEach(function (element) {
+		var $element = $(element);
+
+		// create a link handle for this element
+		$element.on("click", function () {			
+			var $content,
+				$input,
+				$button,
+				i;
 
 			$(".tabs a span").removeClass("active");
 			$element.addClass("active");
 			$("main .content").empty();
 
 			if ($element.parent().is(":nth-child(1)")) {
-				console.log("First tab clicked!");
+				$content = $("<ul>");
+				for(i = toDos.length-1; i >= 0; i--) {
+					$content.append($("<li>").text(toDos[i]));
+				}
 			} else if ($element.parent().is(":nth-child(2)")) {
-				console.log("Second tab clicked!");
+				$content = $("<ul>");
+				toDos.forEach(function (todo) {
+					$content.append($("<li>").text(todo));
+				});
 			} else if ($element.parent().is(":nth-child(3)")) {
-				console.log("Third tab clicked!");
+				$input = $("<input>");
+				$button = $("<button>").text("+");
+
+				$button.on("click", function() {
+					if ($input.val() != "") {
+						toDos.push($input.val());
+						$input.val("");
+					}
+				});
+
+				$content = $("<div>").append($input).append($button);
 			}
+
+			$("main .content").append($content);
 
 			return false;
 		});
 	});
+
+	$(".tabs a:first-child span").trigger("click");
 };
 
 $(document).ready(main);
